98c160e4c87e1f6dc7add2d6dd4541c582709c4d
Jalante4 <143284004+Jalante4@users.noreply.github.com>
2023-08-28 22:41:02 -0400

    Add files via upload
    Grid randomizer wih user input

--- Gridrandom.py
+++ Gridrandom.py
@@ -1,7 +1,14 @@
 import random
 
-row = random.randint(1, 200)
+max_row = input("Whats the amount of rows? ")
+max_row = int(max_row)
+
+max_col = input("Whats the amount of colums? ")
+max_col = int(max_col)
+
+
+row = random.randint(1, max_row)
 print("Row: ",row)
 
-col = random.randint(1, 10)
+col = random.randint(1, max_col)
 print("Column: ", col)
